32ddb928096175448d7852bd9ec18d9c57925e61
Umaisrehman-svg <umaisrehman993@gmail.com>
2026-03-11 04:01:20 +0000

    Delete Lab 1/Lab work/Lab 1..cpp

--- Lab 1/Lab work/Lab 1..cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>      //  use for input and output
-
-using namespace std;     // use for writing cout instead of std::cout
-
-int main()               // Main function where program starts
-{
-    cout << "Hello World";   // Prints Hello World on the screen
-    return 0;                // Ends the program
-}
