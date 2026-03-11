fe6aa5b64e7551bb41e04122e0a5c2b7a89edafb
Umaisrehman-svg <umaisrehman993@gmail.com>
2026-03-11 04:49:53 +0000

    Add files via upload

---
+++ Lab 2/Lab Task/LAB_2 TASK_7.cpp
@@ -0,0 +1,17 @@
+// Name: umais
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0729
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // loop to print name 4 times
+    for (int i = 1; i < 5; i++)
+    {
+        cout << "Talha Ali" << endl;  // print name
+    }
+
+    return 0;  // program ends
+}
